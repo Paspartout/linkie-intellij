bccec60a33db2f4d548c7ee99e3ac1bb34095b89
GitHub Action <action@github.com>
2021-02-10 21:47:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/paspartout/linkieintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/paspartout/linkieintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.paspartout.linkieintellij.services
 
+import com.github.paspartout.linkieintellij.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
